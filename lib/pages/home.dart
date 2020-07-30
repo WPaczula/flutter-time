b048b0d0ccb787d43e83651ae92a6f8cff15fbb5
WojtekP <wojciech.paczula@gmail.com>
2020-07-30 13:21:52 +0200

    feat: Add day night theme difference

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_time/constants/argument_keys.dart';
 import 'package:flutter_time/models/location_time.dart';
 import 'package:flutter_time/services/date_format_service.dart';
+import 'package:flutter_time/services/day_night_service.dart';
 import 'package:flutter_time/services/flag_service.dart';
 import 'package:flutter_time/services/location_name_service.dart';
 
@@ -15,6 +16,7 @@ class _HomeState extends State<Home> {
   FlagService flagService = FlagService();
   LocationNameService locationNameService = LocationNameService();
   DateFormatService dateFormatService = DateFormatService();
+  DayNightService dayNightService = DayNightService();
 
   @override
   Widget build(BuildContext context) {
@@ -22,35 +24,44 @@ class _HomeState extends State<Home> {
     LocationTime locationTime = arguments[locationTimeKey];
     String locationName = locationNameService.getName(locationTime.location);
     String time = dateFormatService.formatTime(locationTime.dateTime);
+    bool isDayTime = dayNightService.isDay(locationTime.dateTime);
+
+    Color textColor = isDayTime ? Colors.black : Colors.white;
+    Color backgroundColor = isDayTime ? Colors.white : Colors.blueGrey[900];
 
     return Scaffold(
+        backgroundColor: backgroundColor,
         body: SafeArea(
             child: Padding(
-      padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-      child: Column(
-        children: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text("Edit location")),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
             children: <Widget>[
-              Text(locationName,
-                  style: TextStyle(fontSize: 28, letterSpacing: 2)),
+              FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text(
+                    "Edit location",
+                    style: TextStyle(color: textColor),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(locationName,
+                      style: TextStyle(
+                          fontSize: 28, letterSpacing: 2, color: textColor)),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(time, style: TextStyle(fontSize: 66, color: textColor))
             ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(time, style: TextStyle(fontSize: 66))
-        ],
-      ),
-    )));
+        )));
   }
 }
---
+++ lib/services/day_night_service.dart
@@ -0,0 +1,7 @@
+class DayNightService {
+  DayNightService();
+
+  bool isDay(DateTime dateTime) {
+    return dateTime.hour > 6 && dateTime.hour < 20;
+  }
+}
